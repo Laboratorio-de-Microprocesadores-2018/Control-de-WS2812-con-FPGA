a88799065eb1374e8b8af97d85bded08d40c9d60
tlifschitz <tlifschitz@itba.edu.ar>
2018-11-06 20:41:07 -0300

    Driver

--- Testbench Kinetis/source/LedMatrix.c
+++ Testbench Kinetis/source/LedMatrix.c
@@ -1,6 +1,7 @@
 #include "LedMatrix.h"
 #include "SPI.h"
 #include "DMA.h"
+#include "DMAMUX.h"
 #include "SysTick.h"
 #include "Assert.h"
 #include "string.h"
@@ -9,19 +10,32 @@
 //                       Constants and macro definitions                       //
 /////////////////////////////////////////////////////////////////////////////////
 
-#define MAX_BUFFER_LEN 40
+#define TEXT_BUFFER_LEN 40
+#define NUMBER_OF_LEDS (MATRIX_WIDTH * MATRIX_HEIGHT)
+#define SCREEN_BUFFER_SIZE  (NUMBER_OF_LEDS * 3)
+
+/////////////////////////////////////////////////////////////////////////////////
+//                    Enumerations, structures and typedefs                    //
+/////////////////////////////////////////////////////////////////////////////////
+typedef enum {
+	WRITE_MEMORY    = 0x07,
+	UPDATE_NORMAL   = 0x10,
+	UPDATE_MIRRORED = 0x11
+}Command;
 
 /////////////////////////////////////////////////////////////////////////////////
 //                   Local variable definitions ('static')                     //
 /////////////////////////////////////////////////////////////////////////////////
 
-static char TEXT[MAX_BUFFER_LEN];
-
-static uint8_t SCREEN[MATRIX_WIDTH * MATRIX_HEIGHT * 3];
-static uint8_t screenBufferSize;
-static uint8_t numberOfLeds;
+static char textBuffer[TEXT_BUFFER_LEN];
+static Color screenBuffer[NUMBER_OF_LEDS];
+static Command updateMode;
 static bool screenBusy;
 static bool colorTestRunning;
+static DMA_TransferConfig DMATransfer;
+static void LedMatrix_WriteMemory();
+static void LedMatrix_PartialWriteMemory(uint8_t startAddress, uint8_t length);
+static void LedMatrix_SendUpdate();
 
 /////////////////////////////////////////////////////////////////////////////////
 //                          Global Function Definitions                        //
@@ -31,8 +45,40 @@ void LedMatrix_Init()
 {
 	sysTickInit();
 
-	numberOfLeds = MATRIX_WIDTH * MATRIX_HEIGHT;
-	screenBufferSize =  numberOfLeds * 3;
+	DMA_Config DMAconfig;
+	DMA_GetDefaultConfig(&DMAconfig);
+	DMAconfig.enableDebugMode=false;
+	DMA_Init(&DMAconfig);
+	DMAMUX_Init();
+
+	// Configure DMA0 to copy from screenBuffer to SPI transmitter
+	DMAMUX_SetSource(0,DMAMUX_SPI0_TX);
+	DMAMUX_EnableChannel(0,true);
+
+
+	DMATransfer.sourceAddress = (uint32_t)screenBuffer;
+	DMATransfer.sourceOffset = 1;
+	DMATransfer.sourceTransferSize = DMA_TransferSize1Bytes;
+
+	DMATransfer.destinationAddress = (uint32_t)0;//SPI_GetFIFOAddress();
+	DMATransfer.destinationOffset = 0;
+	DMATransfer.destinationTransferSize = DMA_TransferSize1Bytes;
+
+	DMATransfer.majorLoopCounts = sizeof(screenBuffer)/sizeof(uint8_t);
+	DMATransfer.minorLoopBytes = 1;
+	DMATransfer.sourceLastAdjust = -1*sizeof(screenBuffer)/sizeof(uint8_t);
+	DMATransfer.destinationLastAdjust = 0;
+	DMA_SetTransferConfig(0,&DMATransfer);
+	DMA_EnableChannelRequest (0);
+
+	DMA_SetCallback(0,LedMatrix_SendUpdate);
+
+
+
+
+	SPI_MasterConfig SPIConfig; // Con DMA deshabilitado
+	SPI_MasterGetDefaultConfig(&SPIConfig);
+	SPI_MasterInit(&SPIConfig);
 
 }
 void LedMatrix_Clear()
@@ -42,17 +88,17 @@ void LedMatrix_Clear()
 }
 void LedMatrix_PlainColor(Color c)
 {
-	for(int i=0; i<numberOfLeds; i++)
+	for(int i=0; i<NUMBER_OF_LEDS; i+=3)
 	{
-		SCREEN[i]   = c.G;
-		SCREEN[i+1] = c.R;
-		SCREEN[i+2] = c.B;
+		screenBuffer[i]   = c;
 	}
-	//SPI_StartTransfer();
+	LedMatrix_WriteMemory();
 }
 void LedMatrix_ColorTest()
 {
+	colorTestRunning = true;
 
+	LedMatrix_WriteMemory();
 }
 
 bool LedMatrix_ColorTestRunning()
@@ -62,8 +108,36 @@ bool LedMatrix_ColorTestRunning()
 
 void LedMatrix_Print(char * c, uint8_t len,Color fontColor, Color backgroundColor)
 {
-	ASSERT(len < MAX_BUFFER_LEN);
-	memcpy(c,TEXT,len);
+	ASSERT(len < TEXT_BUFFER_LEN);
+	memcpy(c,textBuffer,len);
 
 }
+
+static void LedMatrix_WriteMemory()
+{
+
+	LedMatrix_PartialWriteMemory(0,NUMBER_OF_LEDS);
+	// SPI_EnableDMAReqests();
+}
+
+static void LedMatrix_PartialWriteMemory(uint8_t startAddress, uint8_t length)
+{
+	ASSERT(startAddress < NUMBER_OF_LEDS);
+	ASSERT(length < NUMBER_OF_LEDS - startAddress);
+
+	SPI_SendByte(WRITE_MEMORY);
+	SPI_SendByte(startAddress);
+
+	DMATransfer.sourceAddress = screenBuffer + startAddress;
+	DMATransfer.majorLoopCounts = length * 3;
+	DMA_SetTransferConfig(0,&DMATransfer);
+	// SPI_EnableDMAReqests();
+}
+
+static void LedMatrix_SendUpdate()
+{
+	//SPI_DisableDMAReqests();
+	SPI_SendByte(updateMode);
+}
+
 
